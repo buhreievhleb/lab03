c01f1965966523960d20ad838d7f298832e8273e
Buhreiev Hleb <buhreiev.hleb@lll.kpi.ua>
2024-01-23 21:19:56 +0200

    lab03

---
+++ lab3_2/app/src/main/java/glib/bugreyev/kp13/lab32/MainActivity.java
@@ -0,0 +1,14 @@
+package glib.bugreyev.kp13.lab32;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
